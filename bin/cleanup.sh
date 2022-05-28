9fee47b6d5b720cbd5c9241786dba85801076085
Jeremiah Orians <jeremiah@pdp10.guru>
2022-05-28 14:15:51 -0400

    clean up bin

---
+++ bin/cleanup.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+# Mes --- Maxwell Equations of Software
+# Copyright © 2020 Jeremiah Orians
+#
+# This file is part of Mes.
+#
+# Mes is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or (at
+# your option) any later version.
+#
+# Mes is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Mes.  If not, see <http://www.gnu.org/licenses/>.
+
+# For keeping the bin directory, despite wanting to ignore it's contents
+
+set -eux
+
+rm -vf AMD64/bin/M1
+rm -vf AMD64/bin/M2-Mesoplanet
+rm -vf AMD64/bin/M2-Planet
+rm -vf AMD64/bin/blood-elf
+rm -vf AMD64/bin/catm
+rm -vf AMD64/bin/chmod
+rm -vf AMD64/bin/cp
+rm -vf AMD64/bin/get_machine
+rm -vf AMD64/bin/hex2
+rm -vf AMD64/bin/kaem
+rm -vf AMD64/bin/match
+rm -vf AMD64/bin/mkdir
+rm -vf AMD64/bin/sha256sum
+rm -vf AMD64/bin/ungz
+rm -vf AMD64/bin/untar
